ac30f289324ca236bdba878593f9de6d06d43212
Sara <sara.milovic95@gmail.com>
2024-10-10 11:23:42 +0200

    Feature: User input

--- MovieFestival/App.cs
+++ MovieFestival/App.cs
@@ -10,5 +10,32 @@ class App
         program.AddMovie(new Movie(60, new Genre("Triler"), "Title 2"));
 		Console.WriteLine(program.GetData());
     }
+
+
+    public Movie CreateMovie()
+    {
+        Console.WriteLine("Enter Movie data:");
+        Console.WriteLine("Enter genre:");
+
+		string genre = Console.ReadLine();
+
+		Console.WriteLine("Enter title:");
+
+		string title = Console.ReadLine();
+
+		Console.WriteLine("Enter duration:");
+		int length = Convert.ToInt32(Console.ReadLine());
+
+        return new Movie(length, new Genre(genre), title);
+	}
+
+	public Program CreateProgram()
+	{
+		Console.WriteLine("Enter Program date:");
+
+		DateTime date = Convert.ToDateTime(Console.ReadLine());
+
+		return new Program(date);
+	}
 }
 

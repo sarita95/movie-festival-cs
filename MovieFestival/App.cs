e1386c917fc00eb55bc09ac4b6ccbf451ab6fdaf
Sara <sara.milovic95@gmail.com>
2024-10-10 11:04:34 +0200

    Feature: Add Program

--- MovieFestival/App.cs
+++ MovieFestival/App.cs
@@ -4,8 +4,11 @@ class App
 {
     static void Main(string[] args)
     {
-        string genre = new Genre("Drama").GetData();
-        Console.WriteLine(genre);
+        var program = new Program(DateTime.Now);
+
+        program.AddMovie(new Movie(55, new Genre("Drama"), "Title 1"));
+        program.AddMovie(new Movie(60, new Genre("Triler"), "Title 2"));
+		Console.WriteLine(program.GetData());
     }
 }
 
---
+++ MovieFestival/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFestival
+{
+	internal class Program
+	{ 
+		public DateTime Date { get; set; }
+		public List<Movie> Movies { get; set; }
+
+		public Program(DateTime date)
+		{
+			Date = date;
+			Movies = new List<Movie>();
+		}
+
+		public void AddMovie(Movie movie)
+		{		
+			Movies.Add(movie); 
+		}
+
+		public string GetData()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine(Date.ToString());
+
+			int length = 0;
+			foreach (Movie movie in Movies)
+			{
+				length += movie.Length;
+				sb.AppendLine(movie.GetData());
+			}
+
+			sb.AppendLine("Duration: " + length.ToString());
+
+			return sb.ToString();
+		}
+	}
+}
